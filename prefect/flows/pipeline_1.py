20cdbd5a074efb2b56e33a368a3c361ebe2163e9
nghuyen <nguyenhaingochuyen2003@gmail.com>
2024-11-07 17:07:45 +0700

    save flow

--- prefect/flows/pipeline_1.py
+++ prefect/flows/pipeline_1.py
@@ -1,35 +1,35 @@
-from prefect import task, flow
+from prefect import task, flow, serve
 from dotenv import load_dotenv
 import os
 import pandas as pd
 import logging
-from prefect.flows.etl.fetch_data import fetch_and_save_movie_data  
+from etl.fetch_data import fetch_and_save_movie_data  
 from etl.transform import MongoDataExtractor  
 from etl.load_data import load_data_to_postgres  
 
 load_dotenv()
 logging.basicConfig(level=logging.INFO)
 
-@task
+@task(retries=2)
 def fetch_movie_data(release_date_from, release_date_to):
     """Fetch movie data and save it to MongoDB."""
     fetch_and_save_movie_data(release_date_from, release_date_to)
 
-@task
+@task(retries=2)
 def transform_data():
     """Transform data from MongoDB into a format suitable for PostgreSQL."""
     extractor = MongoDataExtractor()
     transformed_data = extractor.process_all_collections()
     return transformed_data
 
-@task
+@task(retries=2)
 def load_data(transformed_data):
     """Load transformed data into PostgreSQL."""
     for table_name, data in transformed_data.items():
         if isinstance(data, pd.DataFrame) and not data.empty:
             load_data_to_postgres(data, table_name)
 
-@flow(name="manually-ETL-pipeline")
+@flow(name="manually-ETL-pipeline", log_prints=True)
 def manually_etl_pipeline(release_date_from, release_date_to):
     fetch_movie_data(release_date_from, release_date_to)
     transformed_data = transform_data()
@@ -40,3 +40,4 @@ if __name__ == "__main__":
     manually_etl_pipeline.serve(name="Manually ETL Pipeline",
                                 tags=["pipeline1"],
                                 parameters={"release_date_from": '2024-01-01', "release_date_to": '2024-01-02'})
+    # manually_etl_pipeline('2024-01-01', '2024-01-02')
--- prefect/flows/pipeline_2.py
+++ prefect/flows/pipeline_2.py
@@ -1,4 +1,4 @@
-from prefect import task, flow
+from prefect import task, flow, serve
 from etl.fetch_data import fetch_and_save_movie_data
 from etl.update_data import update_reviews 
 from etl.transform import MongoDataExtractor  
@@ -23,44 +23,44 @@ def connect_mongodb_and_tmdb_api():
     tmdb_api_key = os.getenv('TMDB_API_KEY')
     return db, tmdb_api_key
 
-# Task to fetch reviews and load movies in a week
 @task(retries=2)
 def extract_and_load_recent_movies(release_date_from, release_date_to):
+    """Fetch reviews and ingest movies in a week"""
     fetch_and_save_movie_data(release_date_from, release_date_to)
 
-# Task to check the existence of the top popular movies collection
-@task
-def update_movie_reviews(db, tmdb_api_key, release_date_from, release_date_to):
+@task(retries=2)
+def update_movie_reviews(release_date_from, release_date_to):
+    """Check the existence of the top popular movies collection"""
+    db, tmdb_api_key=connect_mongodb_and_tmdb_api()
     update_reviews(db, tmdb_api_key, release_date_from, release_date_to)
 
-@task
+@task(retries=2)
 def transform_data():
-    """Transform data from MongoDB into a format suitable for PostgreSQL."""
+    """Transform data from MongoDB into a format suitable for PostgreSQL"""
     extractor = MongoDataExtractor()
     transformed_data = extractor.process_all_collections()
     return transformed_data
 
-@task
+@task(retries=2)
 def load_data(transformed_data):
-    """Load transformed data into PostgreSQL."""
+    """Load transformed data into PostgreSQL"""
     for table_name, data in transformed_data.items():
         if isinstance(data, pd.DataFrame) and not data.empty:
             load_data_to_postgres(data, table_name)
 
 @flow(name="ETL-pipeline", log_prints=True)
 def movie_etl_pipeline():
-    # Connect to MongoDB and TMDB API
-    db, tmdb_api_key=connect_mongodb_and_tmdb_api()
-    
-    release_date_from = (datetime.now() - timedelta(days=0)).strftime('%Y-%m-%d')
+    release_date_from = (datetime.now() - timedelta(days=3)).strftime('%Y-%m-%d')
     release_date_to = datetime.now().strftime('%Y-%m-%d')
 
     extract_and_load_recent_movies(release_date_from, release_date_to)
-    update_movie_reviews(db, tmdb_api_key, release_date_from, release_date_to)
+    update_movie_reviews(release_date_from, release_date_to)
     transformed_data = transform_data()
     load_data(transformed_data)
 
 if __name__ == "__main__":
-    """Main ETL pipeline for movie data"""
-    movie_etl_pipeline.serve(name="Movie ETL Pipeline",
-                            tags=["pipeline2"])
+    # """Main ETL pipeline for movie data"""
+    # movie_etl_pipeline.serve(name="Movie ETL Pipeline",
+    #                         tags=["pipeline2"])
+
+    movie_etl_pipeline()
